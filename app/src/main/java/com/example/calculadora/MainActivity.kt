a0372ad30433f95444df5538a4592d993714acb6
angelnavarromesas <angelnavarromesas02@gmail.com>
2023-10-05 22:08:23 +0200

    comiti

--- app/src/main/java/com/example/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.calculadora
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
 import android.view.View
 import com.example.calculadora.databinding.ActivityMainBinding
 import com.example.calculadora.databinding.CalculadoraBinding
@@ -13,6 +12,9 @@ class MainActivity : AppCompatActivity() {
         val main = ActivityMainBinding.inflate(layoutInflater)
         val calculadora = CalculadoraBinding.inflate(layoutInflater)
         setContentView(main.root)
+
+        var usuario: String = "admin"
+        var contraseña: String = "admin"
 
         main.login.setOnClickListener(object : View.OnClickListener{
             override fun onClick(p0: View?) {
@@ -26,11 +28,40 @@ class MainActivity : AppCompatActivity() {
             }
         })
 
+        var result: Double = 0.0
+
         calculadora.botonSumar.setOnClickListener(object : View.OnClickListener{
             override fun onClick(p0: View?) {
-//                calculadora.numResultado.text = calculadora.numero2.text.toString().toDouble()
+                result = calculadora.numero1.text.toString().toDouble() + calculadora.numero2.text.toString().toDouble()
+                calculadora.numResultado.text = result.toString()
+            }
+        })
+        calculadora.botonRestar.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(p0: View?) {
+                result = calculadora.numero1.text.toString().toDouble() - calculadora.numero2.text.toString().toDouble()
+                calculadora.numResultado.text = result.toString()
+            }
+        })
+        calculadora.botonMultiplicar.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(p0: View?) {
+                result = calculadora.numero1.text.toString().toDouble() * calculadora.numero2.text.toString().toDouble()
+                calculadora.numResultado.text = result.toString()
+            }
+        })
+        calculadora.botonDividir.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(p0: View?) {
+                var num1:Double = calculadora.numero1.text.toString().toDouble()
+                var num2:Double = calculadora.numero2.text.toString().toDouble()
+
+                if (num2!=0.0) {
+                    result = num1/num2
+                    calculadora.numResultado.text = result.toString()
+                }
             }
         })
 
     }
+
+
+
 }
