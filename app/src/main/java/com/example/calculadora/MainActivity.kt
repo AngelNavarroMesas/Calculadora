b71d25a294f1823045f0f28aaf2c4584f2317ad7
AngelNavarroMesas <angel.navarro@iesnervion.es>
2023-10-06 14:18:56 +0200

    todo bien, faltan comentarios

--- app/src/main/java/com/example/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -3,22 +3,34 @@ package com.example.calculadora
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import com.example.calculadora.databinding.ActivityMainBinding
 import com.example.calculadora.databinding.CalculadoraBinding
 
 class MainActivity : AppCompatActivity() {
+    var num1: Double = 0.0
+    var num2: Double = 0.0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val main = ActivityMainBinding.inflate(layoutInflater)
         val calculadora = CalculadoraBinding.inflate(layoutInflater)
         setContentView(main.root)
 
-        var usuario: String = "admin"
-        var contraseña: String = "admin"
+        val usuario: String = "admin"
+        val contraseña: String = "admin"
 
         main.login.setOnClickListener(object : View.OnClickListener{
             override fun onClick(p0: View?) {
-                setContentView(calculadora.root)
+                if(main.user.text.toString()==usuario && main.pass.text.toString()==contraseña) {
+                    setContentView(calculadora.root)
+                }else{
+                    val toast = Toast.makeText(
+                        applicationContext,
+                        "Contraseña o usuario incorrectos",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
             }
         })
 
@@ -32,28 +44,76 @@ class MainActivity : AppCompatActivity() {
 
         calculadora.botonSumar.setOnClickListener(object : View.OnClickListener{
             override fun onClick(p0: View?) {
-                result = calculadora.numero1.text.toString().toDouble() + calculadora.numero2.text.toString().toDouble()
+                if(calculadora.numero1.text.toString().isEmpty()){
+                    num1=0.0
+                }else{
+                    num1=calculadora.numero1.text.toString().toDouble()
+                }
+                if(calculadora.numero2.text.toString().isEmpty()){
+                    num2=0.0
+                }else{
+                    num2=calculadora.numero2.text.toString().toDouble()
+                }
+
+                result = num1 + num2
                 calculadora.numResultado.text = result.toString()
             }
         })
+
         calculadora.botonRestar.setOnClickListener(object : View.OnClickListener{
             override fun onClick(p0: View?) {
-                result = calculadora.numero1.text.toString().toDouble() - calculadora.numero2.text.toString().toDouble()
+                if(calculadora.numero1.text.toString().isEmpty()){
+                    num1=0.0
+                }else{
+                    num1=calculadora.numero1.text.toString().toDouble()
+                }
+                if(calculadora.numero2.text.toString().isEmpty()){
+                    num2=0.0
+                }else{
+                    num2=calculadora.numero2.text.toString().toDouble()
+                }
+                result = num1 - num2
                 calculadora.numResultado.text = result.toString()
             }
         })
+
         calculadora.botonMultiplicar.setOnClickListener(object : View.OnClickListener{
             override fun onClick(p0: View?) {
-                result = calculadora.numero1.text.toString().toDouble() * calculadora.numero2.text.toString().toDouble()
+                if(calculadora.numero1.text.toString().isEmpty()){
+                    num1=0.0
+                }else{
+                    num1=calculadora.numero1.text.toString().toDouble()
+                }
+                if(calculadora.numero2.text.toString().isEmpty()){
+                    num2=0.0
+                }else{
+                    num2=calculadora.numero2.text.toString().toDouble()
+                }
+                result = num1 * num2
                 calculadora.numResultado.text = result.toString()
             }
         })
+
         calculadora.botonDividir.setOnClickListener(object : View.OnClickListener{
             override fun onClick(p0: View?) {
-                var num1:Double = calculadora.numero1.text.toString().toDouble()
-                var num2:Double = calculadora.numero2.text.toString().toDouble()
+                if(calculadora.numero1.text.toString().isEmpty()){
+                    num1=0.0
+                }else{
+                    num1=calculadora.numero1.text.toString().toDouble()
+                }
+                if(calculadora.numero2.text.toString().isEmpty()){
+                    num2=0.0
+                }else{
+                    num2=calculadora.numero2.text.toString().toDouble()
+                }
 
-                if (num2!=0.0) {
+                if (num2==0.0) {
+                    val toast = Toast.makeText(
+                        applicationContext,
+                        "El 0 no se puede dividir",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }else{
                     result = num1/num2
                     calculadora.numResultado.text = result.toString()
                 }
@@ -62,6 +122,24 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    //private fun noNulo1(): Double {
+    //    val calculadora = CalculadoraBinding.inflate(layoutInflater)
+    //    if(calculadora.numero1.text.toString()==null){
+    //        num1=0.0
+    //    }else{
+    //        num1=calculadora.numero1.text.toString().toDouble()
+    //    }
+    //    return num1
+    //}
 
+    //private fun noNulo2(): Double {
+    //    val calculadora = CalculadoraBinding.inflate(layoutInflater)
+    //    if(calculadora.numero2.text.toString()==null){
+    //        num2=0.0
+    //    }else{
+    //        num2=calculadora.numero2.text.toString().toDouble()
+    //    }
+    //    return num2
+    //}
 
 }
